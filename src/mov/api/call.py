adc510321cbc56174fb70b35722c26bf85fa61b2
hahahellooo <hahahello777@gmail.com>
2024-07-26 14:11:07 +0900

    ing

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -1,4 +1,14 @@
+import requests
+
 def req(dt="20120101"):
+    url = gen_url(dt)
+    r = requests.get(url)
+    code = r.status_code
+    data = r.json()
+    return code, data
+
+
+def gen_url(dt="20120101"):
     base_url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = "6b328d3bbd3cef67be8ac18f563a84e0"
     url = f"{base_url}?key={key}&targetDt={dt}"
@@ -7,4 +17,7 @@ def req(dt="20120101"):
 #req()
 #req("8"*8)
 #req(dt="99999999999999999")
+
+    return url
+
 
