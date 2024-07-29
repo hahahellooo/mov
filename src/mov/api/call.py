3a3bce3444754ff0b8f5221565298a186c5fe5df
hahahellooo <hahahello777@gmail.com>
2024-07-29 16:35:28 +0900

    fix

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -6,7 +6,7 @@ import pandas as pd
 def save2df(load_dt='20120101'):
     """airflow 호출 지점"""
     df = list2df(load_dt)
-    # df 에 ldad_dt 컬럼 추가 (조회 일자 YYYYMMDD 형식으로)
+    # df 에 load_dt 컬럼 추가 (조회 일자 YYYYMMDD 형식으로)
     # 아래 파일 저장시 load_dt 기준으로 파티셔닝
     df['load_dt'] = load_dt
     df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
