68120e6cdc55dc3f1fba2bd6df825f56ab9db968
hahahellooo <hahahello777@gmail.com>
2024-07-29 15:49:28 +0900

    fix

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -8,7 +8,7 @@ def save2df(load_dt='20120101'):
     df = list2df(load_dt)
     # df 에 ldad_dt 컬럼 추가 (조회 일자 YYYYMMDD 형식으로)
     # 아래 파일 저장시 load_dt 기준으로 파티셔닝
-    df['load_dt'] = 'load_dt'
+    df['load_dt'] = load_dt
     df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
     print(df.head(5))
     return df
